6a2244bb057981b4455f4db31ac3de7bf37d977e
NICCANAS <nic.canas@duocuc.cl>
2023-10-24 14:13:32 -0300

    array jsons y json con datos para "Probar"

---
+++ py/cambiojson.py
@@ -0,0 +1,16 @@
+import json
+import os
+
+#URL CARPETA DE JSONS
+carpeta = 'URL DEL ARCHIVO JSON'
+
+combined_data = []
+
+for archivo in os.listdir(carpeta):
+    if archivo.endswith('.json'):
+        with open(os.path.join(carpeta, archivo), 'r') as f:
+            data = json.load(f)
+        combined_data.append(data)
+
+with open('resultado2.json', 'w') as f:
+    json.dump(combined_data, f, indent=4)
